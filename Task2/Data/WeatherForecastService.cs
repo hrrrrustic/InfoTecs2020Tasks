1d47b35ae1cfaf58eab2fc9a9b7e979882f659f5
Vlad <hrrrrustic@gmail.com>
2020-02-03 11:30:11 +0300

    cleanup and little progress 2 task

--- Task2/Data/WeatherForecastService.cs
+++ Task2/Data/WeatherForecastService.cs
@@ -6,14 +6,14 @@ namespace Task2.Data
 {
     public class WeatherForecastService
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries =
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            var rng = new Random();
+            Random rng = new Random();
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
